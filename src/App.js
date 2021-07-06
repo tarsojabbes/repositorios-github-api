1f09985791a8046f4fbc5a246d25d3165b6d4c96
Tarso Jabbes <tarsojabbes@gmail.com>
2021-07-06 19:05:42 -0300

    Funcionalidades, CSS e Renderização condicional

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
 
   const [usuario, setUsuario] = useState('') // Armazena os valores do input Usuário
   const [repositorios, setRepositorios] = useState([])
+  const [username, setUsername] = useState('')
 
   const handlePesquisa = () => {
     axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => { // Faz o request na API sobre os repositórios do Usuário inserido no Input
@@ -12,6 +13,7 @@ export default function App() {
       const repositoriesName = [] // Criado para atribuir valores a ele
       repositories.map((repository) => repositoriesName.push(repository.name)) // Para cada nome de repositório dos repositórios da API adicionar o nome em repositoriesName
       localStorage.setItem('repositoriesName', repositoriesName)
+      setUsername(usuario)
 
       if (localStorage.getItem('repositoriesName') !== null) {
         const nomeArray = localStorage.getItem('repositoriesName').split(',')
@@ -27,19 +29,29 @@ export default function App() {
 
     <>
       <div id="landing">
-        <h1>Repositórios do Github</h1>
+        <div className="wrapper">
+          <div className="typing-demo">
+            <h1>Repositórios do Github</h1>
+          </div>
+        </div>
         <div id="campo">
           <div className="form-floating mb-3">
-            <input className="form-control" id="floatingInput" placeholder="Usernname" onChange={(e) => setUsuario(e.target.value)}
+            <input className="form-control"
+              id="floatingInput"
+              placeholder="Usernname"
+              onChange={(e) => setUsuario(e.target.value)}
               value={usuario} />
             <label for="floatingInput">Username</label>
           </div>
 
           <button type='button' onClick={handlePesquisa}>Pesquisar</button>
+
         </div>
+        <p>Após clicar em "Pesquisar" os resultados estarão disponíveis abaixo!</p>
       </div>
 
       <div id="repositorios">
+        <h1>{usuario === '' ? "Nenhum username informado" : `Repositórios encontrados para ${username}`}</h1>
         <div id="box-repo">
           {repositorios.map((nome) => <li>{nome.split('-').join(" ").split("_").join(" ")}</li>)}
         </div>
